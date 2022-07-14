635e9899af8e28af1f817279663b33530ceab5c7
tyspice <tyner27@gmail.com>
2022-07-13 22:56:14 -0700

    fix test endpoint with struct tags

--- models/models.go
+++ models/models.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Test struct {
-	ID    primitive.ObjectID
-	name  string
-	stoke string
+	ID    primitive.ObjectID `bson:"_id" json:"id"`
+	Name  string             `bson:"name" json:"name"`
+	Stoke string             `bson:"stoke" json:"stoke"`
 }
